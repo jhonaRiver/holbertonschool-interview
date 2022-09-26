d788ae835e6734c27c04cb9693408a615572a202
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-26 10:15:36 -0400

    Added README and header files

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,12 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+/*System libraries*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*Function prototypes*/
+int is_palindrome(unsigned long n);
+
+#endif /*PALINDROME_H*/
