a999be3a411df19e806c53f407c7bf7044190f70
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-26 10:33:14 -0400

    Added file task 0

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks whether or not a given unsigned integer is a
+ *				   palindrome
+ *
+ * @n: number to be checked
+ * Return: 1 if n is a palindrome and 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	int num = n;
+	int digit, rev_num;
+
+	rev_num = 0;
+	while (n > 0)
+	{
+		digit = n % 10;
+		rev_num = (rev_num * 10) + digit;
+		n /= 10;
+	}
+	if (rev_num == num)
+		return (1);
+	return (0);
+}
