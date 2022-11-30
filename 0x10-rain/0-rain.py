57b97a9e6b126db394a3f5ccac273011b83ecdbd
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-30 14:09:52 -0400

    Added initial files

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module rain."""
+
+
+def rain(walls):
+    """
+    Calculate how many square units of water will be retained after it rains.
+
+    Args:
+        walls (list): contains non-negative integers
+    Returns:
+        total amount of rainwater retained or 0
+    """
