0ea9c9e0eb6984f22527a4b7e7cc0d70939cfee8
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-12-13 14:01:31 -0400

    Added intial files

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located or -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{}
+
+/**
+ * print_array - ...
+ *
+ * @array: ...
+ * @first: ...
+ * @last: ...
+ */
+void print_array(int *array, int first, int last)
+{}
+
+/**
+ * recursive_search - ...
+ *
+ * @array: ...
+ * @first: ...
+ * @last: ...
+ * @value: ...
+ * Return: ...
+ */
+int recursive_search(int *array, size_t first, size_t last, int value)
+{}
