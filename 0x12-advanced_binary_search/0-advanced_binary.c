71b9173207c8d0027df747cdeeaf6c54b178c60a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-12-14 17:51:35 -0400

    Fixed betty

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -10,13 +10,13 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t first, last;
+	size_t first, last;
 
-    if (!array)
-        return (-1);
-    first = 0;
-    last = size - 1;
-    return (recursive_search(array, first, last, value));
+	if (!array)
+		return (-1);
+	first = 0;
+	last = size - 1;
+	return (recursive_search(array, first, last, value));
 }
 
 /**
@@ -28,12 +28,12 @@ int advanced_binary(int *array, size_t size, int value)
  */
 void print_array(int *array, int first, int last)
 {
-    int i;
+	int i;
 
-    printf("Searching in array: ");
-    for (i = first; i < last; i++)
-        printf("%d, ", array[i]);
-    printf("%d\n", array[i]);
+	printf("Searching in array: ");
+	for (i = first; i < last; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
 }
 
 /**
@@ -47,20 +47,20 @@ void print_array(int *array, int first, int last)
  */
 int recursive_search(int *array, size_t first, size_t last, int value)
 {
-    size_t half;
+	size_t half;
 
-    if (first < last)
-    {
-        half = first + (last - first) / 2;
-        print_array(array, (int)first, (int)last);
-        if (array[half] >= value)
-            return (recursive_search(array, first, half, value));
-        else
-            return (recursive_search(array, half + 1, last, value));
-        return ((int)(half));
-    }
-    if (array[first] == value)
-        return (first);
-    print_array(array, (int)first, (int)last);
-    return (-1);
+	if (first < last)
+	{
+		half = first + (last - first) / 2;
+		print_array(array, (int)first, (int)last);
+		if (array[half] >= value)
+			return (recursive_search(array, first, half, value));
+		else
+			return (recursive_search(array, half + 1, last, value));
+		return ((int)(half));
+	}
+	if (array[first] == value)
+		return (first);
+	print_array(array, (int)first, (int)last);
+	return (-1);
 }
