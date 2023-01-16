5dc5368d29c415db7634403a817f24b362877e3f
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-16 14:07:46 -0400

    Added file task 0

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,3 +9,11 @@ def rotate_2d_matrix(matrix):
     Args:
         matrix (list): matrix to rotate
     """
+    n = len(matrix)
+    order = []
+    for i in range(n):
+        for j in range(n - 1, -1, -1):
+            order.append(matrix[j][i])
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = order.pop(0)
