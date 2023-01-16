07036841fb1aacde33824d58986bdbc927f162ae
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-16 13:12:00 -0400

    Added initial file

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module rotate_2d_matrix."""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a matrix 90 degrees clockwise.
+
+    Args:
+        matrix (list): matrix to rotate
+    """
