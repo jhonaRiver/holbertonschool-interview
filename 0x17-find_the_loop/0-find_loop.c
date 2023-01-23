423786bf80dd22c2ab138d18413a0fc0647fed56
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-23 19:31:30 -0400

    Fixed betty

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -8,26 +8,26 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *fast, *slow;
+	listint_t *fast, *slow;
 
-    if (!head)
-        return (NULL);
-    fast = slow = head;
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = (fast->next)->next;
-        if (fast == slow)
-        {
-            slow = head;
-            while (slow && fast)
-            {
-                if (slow == fast)
-                    return (slow);
-                slow = slow->next;
-                fast = fast->next;
-            }
-        }
-    }
-    return (NULL);
+	if (!head)
+		return (NULL);
+	fast = slow = head;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = (fast->next)->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow && fast)
+			{
+				if (slow == fast)
+					return (slow);
+				slow = slow->next;
+				fast = fast->next;
+			}
+		}
+	}
+	return (NULL);
 }
