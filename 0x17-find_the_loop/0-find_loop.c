9248ac228a2ed97e68819ea5cf961664ec68733e
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-23 19:28:40 -0400

    Added file task 0

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -7,4 +7,27 @@
  * Return: address of the node where the loop starts or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
-{}
+{
+    listint_t *fast, *slow;
+
+    if (!head)
+        return (NULL);
+    fast = slow = head;
+    while (slow && fast && fast->next)
+    {
+        slow = slow->next;
+        fast = (fast->next)->next;
+        if (fast == slow)
+        {
+            slow = head;
+            while (slow && fast)
+            {
+                if (slow == fast)
+                    return (slow);
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+    }
+    return (NULL);
+}
