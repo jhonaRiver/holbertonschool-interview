67a5a7d2b2555b9b82d7dd861bb45c2eeb1839b9
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-17 08:30:54 -0400

    Added initial file

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module island_perimeter."""
+
+
+def island_perimeter(grid):
+    """Return the perimeter of the island.
+
+    Args:
+        grid (list): list of list of integers
+    """
